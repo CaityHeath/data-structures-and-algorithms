0852f775e8d76396086ab4aeef54fa1859b4c0eb
CaityHeath <caity.heath@gmail.com>
2018-12-18 14:44:34 -0800

    added 2 more tests. 3/3 tests pass.

--- coding-challenges/401/arrayShift/array-shift.test.js
+++ coding-challenges/401/arrayShift/array-shift.test.js
@@ -9,4 +9,12 @@ describe('shift module', () => {
   it('can add a value to the center of an array', () => {
     expect(shift.insertShiftArray(x, z)).toEqual([ 2, 4, 5, 6, 8]);
   });
+
+  it('checks if the arr parameter is an array', () =>{
+    expect(shift.insertShiftArray(4,4)).toEqual(null);
+  });
+
+  it('if the input array is empty the only value is the parameter value', () => {
+    expect(shift.insertShiftArray([], 5)).toEqual([5]);
+  })
 });
