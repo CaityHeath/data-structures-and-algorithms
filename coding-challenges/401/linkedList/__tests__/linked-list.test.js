40b9e35809ec9aab977c33fa2f43931a47b783a3
CaityHeath <caity.heath@gmail.com>
2018-12-26 18:54:21 -0800

    append, insert, and includes working with tests

---
+++ coding-challenges/401/linkedList/__tests__/linked-list.test.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const LinkedList = require('../linked-list.js');
+
+describe('testing LinkedList', () => {
+  it('append(value) should append a value to an empty linked list', () => {
+    let list = new LinkedList();
+    list.append(1);
+
+    expect(list.head.value).toEqual(1);
+    
+  });
+  
+  it('append(value) should throw a Type Error if value is not a number', () => {
+    let list = new LinkedList();
+
+    expect(() => {
+      list.append('mabel');
+    }).toThrow();
+  });
+
+
+  it('append(value) should append a value to a Linked List that is not empty', () => {
+    let list = new LinkedList();
+    list.append(1);
+    list.append(2);
+    list.append(3);
+    list.append(4);
+    console.log(list);
+
+    expect(list.head.value).toEqual(1);
+    expect(list.head.next.value).toEqual(2);
+    expect(list.head.next.next.value).toEqual(3);
+    expect(list.head.next.next.next.value).toEqual(4);
+    expect(list.head.next.next.next.next).toBeNull();
+  });
+
+
+  it('insert(value) should add a node to the front of an empty Linked List', () => {
+    let list = new LinkedList();
+    list.insert(1);
+
+    expect(list.head.value).toEqual(1);
+    expect(list.head.next).toBeNull();
+  });
+  it('insert(value) should add a node to the front of a Linked List that is not empty', () => {
+    let list = new LinkedList();
+    list.insert(4);
+    list.insert(3);
+    list.insert(2);
+    list.insert(1);
+
+
+    expect(list.head.value).toEqual(1);
+    expect(list.head.next.value).toEqual(2);
+    expect(list.head.next.next.value).toEqual(3);
+    expect(list.head.next.next.next.value).toEqual(4);
+    expect(list.head.next.next.next.next).toBeNull();
+  });
+
+  it('includes(value) should return a boolean result depending on whether the input value exists as a Nodes value', () => {
+    let list = new LinkedList();
+    list.append(1);
+    list.append(2);
+    
+    expect(list.includes(1)).toEqual(true);
+    expect(list.includes(2)).toEqual(true);
+  });
+
+  it('includes(value) should return false if it is not a value in the LinkedList', () => {
+    let list = new LinkedList();
+    list.append(1);
+    list.append(2);
+    
+    expect(list.includes(3)).toBeUndefined();
+  });
+});
