191e4be27e2cfd46aa49ac8d97392915574d9337
CaityHeath <caity.heath@gmail.com>
2019-02-25 21:56:24 -0800

    recursion

---
+++ coding-challenges/401/linked_lists/reverse.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let LinkedList = require('../linked_lists/linkedList/linked-list.js');
+let Node = require('../linked_lists/linkedList/lib/node.js');
+
+
+function Reverse(ll){
+  let next;
+  let prev;
+  let node = new Node()
+  
+}
